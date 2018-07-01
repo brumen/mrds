5e9c53fa48316f466f96413932b66e57efd3c517
brumen <gorazd.brumen@gmail.com>
2018-07-01 10:15:42 -0400

    Fixes for python3

--- pricers/pricers_setup.py
+++ pricers/pricers_setup.py
@@ -1,13 +1,9 @@
-import config 
+# compilation file for pricers_fast
+
 from distutils.core import setup
 from distutils.extension import Extension
-import cython 
 from Cython.Distutils import build_ext
 
-ext_modules = [Extension("pricers_fast", ["pricers_fast.pyx"])]
-
-setup(name='Fast trivariate pricers module',
-      cmdclass={'build_ext': build_ext},
-      ext_modules=ext_modules)
-
-# compile using python pricers_setup.py build_ext --inplace
+setup( name        = 'Fast trivariate pricers module'
+     , cmdclass    = {'build_ext': build_ext}
+     , ext_modules = [Extension("pricers_fast", ["pricers_fast.pyx"])])
