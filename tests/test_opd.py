e2282bd1971bb14186978c82ba80ae28f011f2a1
brumen <gorazd.brumen@gmail.com>
2018-12-31 11:23:32 -0500

    Fixes of testing.

--- tests/test_opd.py
+++ tests/test_opd.py
@@ -1,19 +1,54 @@
-from opd import opd_avx
 import numpy as np
+import pycuda.gpuarray as gpa
+
 from unittest import TestCase
+
+from opd import opd_avx
+from opd.opd_1fuel_cu import getOpdHeader
 
 
 class TestOpd(TestCase):
 
-    def setUp(self):
+    N = 128000
+    r = np.random.rand(N)
+    a = np.random.rand(N)
+    b = np.random.rand(N)
+    c = np.random.rand(N)
+    d = np.random.rand(N)
+    y = np.empty(N)
 
-        self.N = 128000
-        self.r = np.random.rand(self.N)
-        self.a = np.random.rand(self.N)
-        self.b = np.random.rand(self.N)
-        self.c = np.random.rand(self.N)
-        self.d = np.random.rand(self.N)
-        self.y = np.empty(self.N)
+    def sampleInputs(self):
+        """
+        Sample cuda params.
+
+        """
+
+        sampleParams = ( 6.     # hrAtMax
+                       , 7.     # hrAtMin
+                       , 1000.  # maxCap - maximum capacity
+                       , 100.   # minDisp - minimum dispatch
+                       , 10.    # startFuel - startup fuel
+                       , 15.    # startFuelCold - startup fuel from cold.
+                       , 5.     # addFuelCost - added fuel costs
+                       , 10.    # VC - variable costs
+                       , 3.     # rampRate - ramp rate
+                       , 0.1    # shutdownSPin - shutdown shadow price in
+                       , 8.     # minDownTime - minimum downtime
+                       , 16.    # minRunTime - minimum run time.
+                       , 10.    # fixedStartupCost
+                       , 5.     # maxMonthlyStarts
+                       , 10.    # coldStartup
+                       , 16.    # startupHorizon
+                       , 16.    # shutdownHorizon
+                       , 10.    # rampUpSPin
+                       , 10.    # rampDownSPin
+                       , 10.    # rampUpCost
+                       , 10.    # rampDownCost
+                       , 15.    # rampUpHorizon
+                       , 25.    # rampDownHorizon
+                       )
+
+        return sampleParams
 
     def test_just_run(self):
         """
@@ -21,8 +56,124 @@ class TestOpd(TestCase):
 
         """
 
-        y1 = np.empty(self.N)
-        opd_avx.add4(self.r, self.a, self.b, self.c, self.d, y1, self.N)
-        y2 = self.r - (self.a + self.b + self.c + self.d)
+        y1 = np.empty(TestOpd.N)
+        opd_avx.add4(TestOpd.r, TestOpd.a, TestOpd.b, TestOpd.c, TestOpd.d, y1, TestOpd.N)
 
-        self.assertTrue(y1 == y2)
+        self.assertEqual( y1
+                        , TestOpd.r - (TestOpd.a + TestOpd.b + TestOpd.c + TestOpd.d))
+
+    def test_opd_1fuel(self):
+        """
+        Tests the 1 fuel dispatch algorithm.
+
+        """
+
+        N = TestOpd.N
+
+        powerPrices = np.random.rand(N)
+        fuelPrices  = np.random.rand(N)
+        startupSPin = np.random.rand(N)
+        stateState  = np.random.binomial(2, 0.5, N).astype(np.bool)  # TODO: first argument is NOT RIGHT
+        hoursInState= np.random.binomial(10, 0.5, N)
+        stateGeneration = np.random.rand(N) * 10
+        stateTotalStarts = np.random.binomial(3, 0.5, N)
+        stateHoursShut   = np.random.binomial(3, 0.5, N)
+        stateHoursRun    = np.random.binomial(3, 0.5, N)
+        stateGlobalStarts= np.random.binomial(3, 0.5, N)
+        dcCanStart = np.random.binomial(2, 0.5, N).astype(np.bool)
+        dcCanShut = np.random.binomial(2, 0.5, N).astype(np.bool)
+        dcForceStart = np.random.binomial(2, 0.5, N).astype(np.bool)
+        dcForceShut = np.random.binomial(2, 0.5, N).astype(np.bool)
+        hoursInBlock = np.random.binomial(5, 0.5, N)
+
+        opd.opd_1fuel_cu.opd_kernel( 0  # block number
+                  , powerPrices            # FLOAT_TYPE *power prices
+                  , fuelPrices             # FLOAT_TYPE *fuel prices
+                  , self.sampleInputs()    # FLOAT_TYPE * opdParams,
+                  , startupSPin            # FLOAT_TYPE * startupSPin,
+                  , stateState             # bool * state_state,
+                  , hoursInState           # int * state_hoursInState,
+                  , stateGeneration        # FLOAT_TYPE * state_Generation,
+                  , stateTotalStarts       # int * state_TotalStarts,
+                  , stateHoursShut         # int * state_hoursShut,
+                  , stateHoursRun          # int * state_hoursRun,
+                  , stateGlobalStarts      # int * state_globalStarts,
+                  , dcCanStart             # bool * dc_canStart,
+                  , dcCanShut              # bool * dc_canShut,
+                  , dcForceStart           # int * dc_forceStart,
+                  , dcForceShut            # int * dc_forceShut,
+                  , hoursInBlock           # int hours_in_block,
+                  , 0.99                   # FLOAT_TYPE df
+                  , N             )        # int nb_paths,
+
+        self.assertTrue(True)
+
+    def test_2_opd(self):
+        N = 10
+
+        sampleParams = (6.  # hrAtMax
+                        , 7.  # hrAtMin
+                        , 1000.  # maxCap - maximum capacity
+                        , 100.  # minDisp - minimum dispatch
+                        , 10.  # startFuel - startup fuel
+                        , 15.  # startFuelCold - startup fuel from cold.
+                        , 5.  # addFuelCost - added fuel costs
+                        , 10.  # VC - variable costs
+                        , 3.  # rampRate - ramp rate
+                        , 0.1  # shutdownSPin - shutdown shadow price in
+                        , 8.  # minDownTime - minimum downtime
+                        , 16.  # minRunTime - minimum run time.
+                        , 10.  # fixedStartupCost
+                        , 5.  # maxMonthlyStarts
+                        , 10.  # coldStartup
+                        , 16.  # startupHorizon
+                        , 16.  # shutdownHorizon
+                        , 10.  # rampUpSPin
+                        , 10.  # rampDownSPin
+                        , 10.  # rampUpCost
+                        , 10.  # rampDownCost
+                        , 15.  # rampUpHorizon
+                        , 25.  # rampDownHorizon
+                        )
+
+        powerPrices = np.random.rand(N)
+        fuelPrices = np.random.rand(N)
+        startupSPin = np.random.rand(N)
+        stateState = np.random.binomial(2, 0.5, N).astype(np.bool)  # TODO: first argument is NOT RIGHT
+        hoursInState = np.random.binomial(10, 0.5, N)
+        stateGeneration = np.random.rand(N) * 10
+        stateTotalStarts = np.random.binomial(3, 0.5, N)
+        stateHoursShut = np.random.binomial(3, 0.5, N)
+        stateHoursRun = np.random.binomial(3, 0.5, N)
+        stateGlobalStarts = np.random.binomial(3, 0.5, N)
+        dcCanStart = np.random.binomial(2, 0.5, N).astype(np.bool)
+        dcCanShut = np.random.binomial(2, 0.5, N).astype(np.bool)
+        dcForceStart = np.random.binomial(2, 0.5, N).astype(np.bool)
+        dcForceShut = np.random.binomial(2, 0.5, N).astype(np.bool)
+
+        opd_kernel = getOpdHeader()
+        float_type = np.double
+
+        opd_kernel(np.intc(0)  # block number
+                   , gpa.to_gpu(powerPrices).astype(float_type)  # FLOAT_TYPE *power prices
+                   , gpa.to_gpu(fuelPrices).astype(float_type)  # FLOAT_TYPE *fuel prices
+                   , gpa.to_gpu(np.array(sampleParams)).astype(float_type)  # FLOAT_TYPE * opdParams,
+                   , gpa.to_gpu(startupSPin).astype(float_type)  # FLOAT_TYPE * startupSPin,
+                   , gpa.to_gpu(stateState).astype(np.bool)  # bool * state_state,
+                   , gpa.to_gpu(hoursInState).astype(np.intc)  # int * state_hoursInState,
+                   , gpa.to_gpu(stateGeneration).astype(float_type)  # FLOAT_TYPE * state_Generation,
+                   , gpa.to_gpu(stateTotalStarts).astype(np.intc)  # int * state_TotalStarts,
+                   , gpa.to_gpu(stateHoursShut).astype(np.intc)  # int * state_hoursShut,
+                   , gpa.to_gpu(stateHoursRun).astype(np.intc)  # int * state_hoursRun,
+                   , gpa.to_gpu(stateGlobalStarts).astype(np.intc)  # int * state_globalStarts,
+                   , gpa.to_gpu(dcCanStart).astype(np.bool)  # bool * dc_canStart,
+                   , gpa.to_gpu(dcCanShut).astype(np.bool)  # bool * dc_canShut,
+                   , gpa.to_gpu(dcForceStart).astype(np.intc)  # int * dc_forceStart,
+                   , gpa.to_gpu(dcForceShut).astype(np.intc)  # int * dc_forceShut,
+                   , np.intc(8)  # int hours_in_block,
+                   , np.double(0.99)  # FLOAT_TYPE df
+                   , np.ulonglong(N)  # int nb_paths,
+                   , block=(100, 1, 1)
+                   , grid=(100, 1))  # TODO: FIX THESE HERE!!!
+
+        self.assertTrue(True)
--- tests/test_vols.py
+++ tests/test_vols.py
@@ -1,113 +1,30 @@
 #
-# jw volatilities TESTING 
+# Volatility testing class
 
-import config 
-from numpy import * 
-from scipy import *
-#import scipy.optimize
-#import scipy.integrate
-#import scipy.special
-import scipy.stats 
-#import scipy.optimize 
+import datetime
 
-import vols # class to test 
-import unittest 
+from unittest import TestCase, TestLoader, TextTestRunner
+
+from vols.vols import JW7Volatility
 
 
-# jw parametrization (inherits from vol_param)
-class jw_paramsTest(unittest.TestCase):
-    def test_positive(self):
-        value = vols.jw_params(0.2, 0.1, 0.2, 0.1, 0.02)
-        print value.compute_vol (100,110,1)
-        cnd = True # initial value of the condition 
-        for ind in arange(90,110,1):
-            cnd = cnd & (value.compute_vol (100, ind, 1) > 0)
+class JWVolTest(TestCase):
 
-        self.assertTrue ( cnd )
+    def test_just_run(self):
+        """
+        Tries to run the JW7Volatility class.
 
-    # MISSING TESTS FOR NOT BEING INF OR NAN
+        """
 
+        vol1 = JW7Volatility.fromDb('WTI', datetime.date(2015, 4, 1))
 
-class jw7_paramsTest (unittest.TestCase):
-    # def test_positive(self):
-    #     value = vols.jw7_params(0.2, 0.1, 0.2, 0.1, 0.02, 0.1, 0.2)
-    #     cnd_vols = True # initial value of the condition for vols checking 
-    #     cnd_lv = True # init for local vol
-    #     for ind in arange(90,110,1):
-    #         cnd_vols = cnd_vols & (value.compute_vol (100, ind, 1) > 0)
-    #         cnd_lv = cnd_lv & value.local_vol_jw7_alt ( ind, S, S_0, ttm, disc_fact, T ):
-    #     self.assertTrue ( cnd )
+        self.assertTrue(True)
 
 
-    # checks whether the implied vols are positive (simple test)
-    def test_implied_vol (self):
-        #value = vols.jw7_params (array([[100., 0.2, 1.5, 0.5, 0.1, 0.12, 0.1, 0.2]]))
-        value = vols.jw7_params (array([[100., 0.2, 1.5, 0.5, 0.1, 0.12, 0.1, 0.2], 
-                                        [100., 0.2, 0.5, 1.5, 0.1, 0.12, 0.1, 0.2]]))
-        cnd_iv = True # conditional of the implied vol
-        K_range = arange (80., 130., 1.)
-        imp_v_range = zeros (len (K_range) )
-        for K_ind, K in enumerate(K_range):
-            curr_vol = value.implied_vol (1, K, 0.3)
-            imp_v_range[K_ind] = curr_vol
-            cnd_iv = cnd_iv & (curr_vol > 0)
+def main():
+    """
+    Run the tests.
 
-        print "Implied vols = ", imp_v_range
+    """
 
-        self.assertEqual (cnd_iv,True)
-
-
-    def test_implied_vol_all_fwd (self):
-        #value = vols.jw7_params (array([[100., 0.2, 1.5, 0.5, 0.1, 0.12, 0.1, 0.2]]))
-        value = vols.jw7_params (array([[100., 0.2, 1.5, 0.5, 0.1, 0.12, 0.1, 0.2], 
-                                        [100., 0.2, 0.5, 1.5, 0.1, 0.12, 0.1, 0.2]]))
-        cnd_iv = True # conditional of the implied vol
-        K_range = arange (80., 130., 1.)
-        
-        curr_vol = value.implied_vol_all_fwd (K_range, array([0.4]) )
-        curr_vol_pos = curr_vol > 0.
-        cnd_iv = curr_vol_pos.all()
-
-        print "Implied vols = ", curr_vol
-
-        self.assertEqual (cnd_iv,True)
-
-        
-    # tests the functioning of the skew distribution 
-    def test_skew_distr (self):
-        #value = vols.jw7_params (array([[100., 0.2, 1.5, 0.5, 0.1, 0.12, 0.1, 0.2]]))
-        value = vols.jw7_params (array([[100., 0.2, 1.5, 0.5, 0.1, 0.12, 0.1, 0.2], 
-                                        [100., 0.2, 0.5, 1.5, 0.1, 0.12, 0.1, 0.2]]))
-        cnd_iv = True # conditional of the implied vol
-
-        print value.call_future_T (0, 100., 110., 1.)
-        print value.call_future_K (0, 100., 110., 1.)
-        print value.call_future_KK (0, 100., 110., 1.)
-        print value.skewed_distribution (0, 100., 101., 1.)
-        print value.skewed_cdf (0, 100., 100., 1., 1.)
-        print value.inversion_skewed_cdf (0, 100., 1., 2.5)
-
-        self.assertEqual (cnd_iv,True)
-
-
-    def test_skew_distr_speed (self):
-        #value = vols.jw7_params (array([[100., 0.2, 1.5, 0.5, 0.1, 0.12, 0.1, 0.2]]))
-        value = vols.jw7_params (array([[100., 0.2, 1.5, 0.5, 0.1, 0.12, 0.1, 0.2], 
-                                        [100., 0.2, 0.5, 1.5, 0.1, 0.12, 0.1, 0.2]]))
-        cnd_iv = True # conditional of the implied vol
-
-        quantile_range = arange(0.1, 1., 0.1)
-        for quantile in quantile_range: 
-            qv = value.inversion_skewed_cdf (0, 100., 1., quantile)
-            print "quantile = ", quantile, " solu = ", qv
-
-
-        self.assertEqual (cnd_iv,True)
-
-
-
-    
-
-# running the above tests 
-suite = unittest.TestLoader().loadTestsFromTestCase(jw7_paramsTest)
-unittest.TextTestRunner(verbosity=2).run(suite)
+    TextTestRunner(verbosity=2).run(TestLoader().loadTestsFromTestCase(JWVolTest))
