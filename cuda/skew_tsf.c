a1829ac762f7a05e00bf4f071c16443a4de464d1
brumen <gorazd.brumen@gmail.com>
2019-01-01 12:04:55 -0500

    Makes the function more flexible.

--- cuda/skew_tsf.c
+++ cuda/skew_tsf.c
@@ -1,22 +1,22 @@
 // transformation code 
-__global__ void F_skew_tsf( float F
-                          , float c1
-                          , float c2
-                          , float c3
-                          , float V_u
-                          , float *X_u
-                          , float *res
-                          , int n ) {
+__global__ void F_skew_tsf( FLOAT_TYPE F
+                          , FLOAT_TYPE c1
+                          , FLOAT_TYPE c2
+                          , FLOAT_TYPE c3
+                          , FLOAT_TYPE V_u
+                          , FLOAT_TYPE *X_u
+                          , FLOAT_TYPE *res
+                          , INT_TYPE   n ) {
   // implements:
   // F_res = F_u * (1. + X_u + 0.5 * c1 * (X_u**2 - V_u) +
   //                                     c2 * (X_u**3 - 3. * X_u * V_u) / 6. +
   //                                     c3 * (X_u**4 - 6. * V_u * X_u**2 + 3. * V_u**2) / 24.)
 
-  int tx = blockIdx.x;
-  float xu = X_u[tx];
-  float vu = V_u;
-  float xu2, xu3, xu4, vuxu, vuxu2;
-  float r1 = 1.;
+  INT_TYPE   tx = blockIdx.x;
+  FLOAT_TYPE xu = X_u[tx];
+  FLOAT_TYPE vu = V_u;
+  FLOAT_TYPE xu2, xu3, xu4, vuxu, vuxu2;
+  FLOAT_TYPE r1 = 1.;
   
   if (tx < n) {
     xu2 = xu * xu; 
