dd3e448eff9cfe86881281d52eb046e0aab98d37
brumen <gorazd.brumen@gmail.com>
2019-09-07 19:01:18 -0400

    Fixes of testing.

--- forward_curve.py
+++ forward_curve.py
@@ -5,7 +5,7 @@
 import datetime
 import scipy.interpolate
 
-from typing import List
+from typing import List, Tuple
 
 import ds
 
@@ -33,20 +33,23 @@ class FwdCurve:
         :param dcf: day-count factor.
         """
 
-        self._mkt_date   = mkt_date
+        self.mkt_date   = mkt_date
         self.fwd_name   = fwd_name
         self.fwd_tenors = fwd_tenors
         self.fwd_values = fwd_values
         self._dcf       = dcf
 
-        self.__fwdCurve = None
-        self.__fwdTenorsNumeric = None
+        self.__fwd_curve = None
+        self.__fwd_tenors_numeric = None
 
     def __relative_date(self, fwd_date : datetime.date) -> float:
+        """ Computes the numeric date between fwd_date and self.mkt_date
+
+        :param fwd_date: date from which the numerical distance is computed to mkt_date
+        :returns: numerical distance to mkt date.
         """
 
-        """
-        return (fwd_date - self._mkt_date).days / self._dcf
+        return (fwd_date - self.mkt_date).days / self._dcf
 
     @property
     def _fwdTenorsNumeric(self, dcf=365.25):
@@ -55,70 +58,67 @@ class FwdCurve:
 
         """
 
-        if self.__fwdTenorsNumeric:
-            return self.__fwdTenorsNumeric
+        if self.__fwd_tenors_numeric:
+            return self.__fwd_tenors_numeric
 
-        self.__fwdTenorsNumeric = [self.__relative_date(fwdDate) for fwdDate in self.fwd_tenors]
-        return self.__fwdTenorsNumeric
+        self.__fwd_tenors_numeric = [self.__relative_date(fwdDate) for fwdDate in self.fwd_tenors]
+        return self.__fwd_tenors_numeric
 
-    def _fwdCurve(self):
-        """
-        Constructs the splined forward curve.
-
-        """
-
-        if self.__fwdCurve:
-            return self.__fwdCurve
-
-        self.__fwdCurve = scipy.interpolate.splrep(self._fwdTenorsNumeric, self.fwd_values)
-        return self.__fwdCurve
-
-    def fwdValue(self, fwdDate) -> float:
-        """
-        Gets the forward value for the forward date.
+    def _fwd_curve(self):
+        """ Constructs the splined forward curve internally.
 
         """
 
-        if isinstance(fwdDate, list):  # TODO: list is problematic, as it can be list[float] or list[datetime.date]
-            return scipy.interpolate.splev( [self.__relative_date(fwdDates) for fwdDates in fwdDate]
-                                          , self._fwdCurve)
+        if self.__fwd_curve:
+            return self.__fwd_curve
 
-        if isinstance(fwdDate, datetime.date):
-            return scipy.interpolate.splev(self.__relative_date(fwdDate), self._fwdCurve)
+        self.__fwd_curve = scipy.interpolate.splrep(self._fwdTenorsNumeric, self.fwd_values)
+        return self.__fwd_curve
 
-        if isinstance(fwdDate, float):
-            return scipy.interpolate.splev(fwdDate, self._fwdCurve)
+    def fwd_value(self, fwd_date) -> float:
+        """ Gets the forward value for the forward date fwd_date
 
-        raise FwdCurveException('Forward value {0} not recognized'.format(str(fwdDate)))
-
-    def get_1nb( self, fwdDate  : datetime.date ):
-        """
-        Finds the 1st nearby contract to fwd_date.
-
-        :param fwdDate: forward date to which the 1st nearby contract is searched.
         """
 
-        if fwdDate > self.fwd_tenors[-1]:
-            raise FwdCurveException('Fwd date searched {0} larger than the curve last date {1}'.format(fwdDate, self.fwd_tenors[-1]))
+        if isinstance(fwd_date, list):  # TODO: list is problematic, as it can be list[float] or list[datetime.date]
+            return scipy.interpolate.splev([self.__relative_date(fwd_dates) for fwd_dates in fwd_date]
+                                           , self._fwd_curve)
 
-        largerDates = sum([fwdTenor <= fwdDate for fwdTenor in self.fwd_tenors])
+        if isinstance(fwd_date, datetime.date):
+            return scipy.interpolate.splev(self.__relative_date(fwd_date), self._fwd_curve)
+
+        if isinstance(fwd_date, float):
+            return scipy.interpolate.splev(fwd_date, self._fwd_curve)
+
+        raise FwdCurveException('Forward value {0} not recognized'.format(str(fwd_date)))
+
+    def get_1nb(self, fwd_date  : datetime.date) -> Tuple[datetime.date, float]:
+        """ Finds the 1st nearby contract to fwd_date.
+
+        :param fwd_date: forward date to which the 1st nearby contract is searched.
+        """
+
+        if fwd_date > self.fwd_tenors[-1]:
+            raise FwdCurveException('Fwd date searched {0} larger than the curve last date {1}'.format(fwd_date, self.fwd_tenors[-1]))
+
+        largerDates = sum([fwdTenor <= fwd_date for fwdTenor in self.fwd_tenors])
 
         return self.fwd_tenors[largerDates], self.fwd_values[largerDates]
 
     @classmethod
-    def from_db(cls, mktDate : datetime.date, fwdCurveName : str):
+    def from_db(cls, mkt_date : datetime.date, fwd_curve_name : str):
+        """ Reads the forward curves from the database.
+
+        :param mkt_date: market date
+        :param fwd_curve_name: name of the forward, e.g. 'WTI'
+        :returns: FwdCurve object for market date and curve.
         """
-        Reads the forward curves from the database.
 
-        :param mktDate: market date
-        :param fwdCurveName: name of the forward, e.g. 'WTI'
-        """
+        fwd_vol_matched = ds.read_data_matched_tenors( mkt_date
+                                                     , fwd_curve_name
+                                                     , fwd_curve_name)
 
-        fwd_vol_matched = ds.read_data_matched_tenors(mktDate
-                                                      , fwdCurveName
-                                                      , fwdCurveName)
-
-        return cls( mktDate
-                  , fwdCurveName
-                  , fwd_vol_matched['fwd_tenors_dt']
-                  , fwd_vol_matched['fwd_curve'] )
+        return cls(mkt_date
+                   , fwd_curve_name
+                   , fwd_vol_matched['fwd_tenors_dt']
+                   , fwd_vol_matched['fwd_curve'])
