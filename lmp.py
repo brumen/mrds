ca4f0e5f6cdbe6a531c2c30281f03fdf70b9d55e
brumen <gorazd.brumen@gmail.com>
2019-09-22 17:32:41 -0400

    Refactoring to make it in a form of a class.

--- lmp.py
+++ lmp.py
@@ -2,138 +2,153 @@
 # computes location marginal pricing (LMP)
 #
 
+import copy
 import numpy as np
 from scipy.optimize import linprog
+from typing         import List, Tuple
 
-import copy
 
-
-# nodes in the network are buses
-# variables
-def find_pos_y(node_1, node_2, gl, pl):
-    """
-    find the position of (node_1, node_2) connection in PL
-
-    :param pl: list of (node_1, node_2, c_12)
+class LMP:
+    """ Locational marginal pricing.
     """
 
-    n1, n2, c = pl[0]
-    k = 0
-    while ((node_1, node_2) != (n1, n2)) and ((node_2, node_1) != (n1, n2)):
-        k += 1
-        n1, n2, c = pl[k]
+    def __init__(self, gl : List[Tuple], pl : List[Tuple]):
+        """
+        nodes in the network are buses
+        variables
 
-    return len(gl) + k
+        :param gl: generation list in the form (node, generation, load, price of generation)
+        :param pl: in the form of (node_1, node_2, transmission capacity)
+        """
 
+        self.gl = gl
+        self.pl = pl
 
-def find_incoming(node_nb, pl):
-    """
-    find incoming nodes into node_nb
-    """
+    def __find_pos_y(self, node_1, node_2):
+        """ Find the position of (node_1, node_2) connection in production list pl.
 
-    res_l = []
-    for node_1, node_2, c_12 in pl:
-        if node_2 == node_nb:
-            res_l.append(node_1)
-    return res_l
+        :param pl: list of (node_1, node_2, c_12)
+        """
 
+        n1, n2, c = self.pl[0]
+        k = 0
+        while ((node_1, node_2) != (n1, n2)) and ((node_2, node_1) != (n1, n2)):
+            k += 1
+            n1, n2, c = self.pl[k]
 
-def find_outgoing(node_nb, pl):
-    """
-    find incoming nodes into node_nb
-    """
-    res_l = []
-    for node_1, node_2, c_12 in pl:
-        if node_1 == node_nb:
-            res_l.append(node_2)
-    return res_l
+        return len(self.gl) + k
 
+    def __find_incoming(self, node_nb):
+        """ Find incoming nodes into node_nb in production list pl
 
-def find_connected(node_nb, pl):
-    res_l = []
-    for node_1, node_2, c_12 in pl:
-        if node_1 == node_nb:
-            res_l.append(node_2)
-        if node_2 == node_nb:
-            res_l.append(node_1)
-    return res_l
+        :param node_nb: node to search.
+        """
 
+        res_l = []
+        for node_1, node_2, c_12 in self.pl:
+            if node_2 == node_nb:
+                res_l.append(node_1)
 
-def computeLoadDistribution(gl, pl, show_sol=False):
-    """
-    Computes the value of the optimization problem given gl and pl
+        return res_l
 
-    :param show_sol: shows the solution
+    def __find_outgoing(self, node_nb):
+        """ Find incoming nodes into node_nb
+        """
 
-    """
+        res_l = []
+        for node_1, node_2, c_12 in self.pl:
+            if node_1 == node_nb:
+                res_l.append(node_2)
+        return res_l
 
-    nb_nodes = len(gl)
-    nb_lines = len(pl) # each line counted 2x
-    nb_vars = nb_nodes + nb_lines
+    def __find_connected(self, node_nb):
+        res_l = []
+        for node_1, node_2, c_12 in self.pl:
+            if node_1 == node_nb:
+                res_l.append(node_2)
+            if node_2 == node_nb:
+                res_l.append(node_1)
 
-    A_ineq = []
-    b_ineq = []
-    A_eq = []
-    b_eq = []
+        return res_l
 
-    # lower/upper bound on x, transmission
-    lb = np.empty(nb_vars)  # lower bound
-    lb[:nb_nodes] = 0.
-    ub = np.empty(nb_vars)
-    ub[:nb_nodes] = np.array([gen for (node, gen, load, p) in gl])  # generation upper boundary
-    # line constraints ( abs(y) < c )
-    for node_1, node_2, c in pl:
-        y_pos = find_pos_y(node_1, node_2, gl, pl)
-        lb[y_pos] = -c
-        ub[y_pos] = c
+    def compute_load_distribution(self, show_sol=False):
+        """ Computes the value of the optimization problem given gl and pl
 
-    # node constraints
-    for node_nb, generation, load, gen_price in gl:
-        vt_n = np.zeros(nb_vars)
-        incoming_l = find_incoming(node_nb, pl)
-        outgoing_l = find_outgoing(node_nb, pl)
-        vt_n[node_nb-1] = -1.
-        for node_inc in incoming_l:
-            vt_n[find_pos_y(node_inc, node_nb, gl, pl)] = -1.
-        for node_out in outgoing_l:
-            vt_n[find_pos_y(node_nb, node_out, gl, pl)] = 1.
-        A_ineq.append(vt_n)
-        b_ineq.append(-load)
+        :param show_sol: shows the solution
+        """
 
-    # optimization vector - prices over nodes
-    opt_vec = np.zeros(nb_vars)
-    opt_vec[:nb_nodes] = np.array([p for (node_nb, gen, load, p) in gl])
+        nb_nodes = len(self.gl)
+        nb_lines = len(self.pl) # each line counted 2x
+        nb_vars = nb_nodes + nb_lines
 
-    if A_eq:  # linprog cannon handle empty matrices
-        problem = linprog( opt_vec, A_ub=np.array(A_ineq), b_ub=np.array(b_ineq), A_eq=A_eq, b_eq=b_eq
-                         , bounds = list(zip(lb, ub)) )
-    else:
-        problem = linprog( opt_vec, A_ub=np.array(A_ineq), b_ub=np.array(b_ineq)
-                         , bounds=list(zip(lb, ub)) )
+        A_ineq = []
+        b_ineq = []
+        A_eq = []
+        b_eq = []
 
-    sol_v = problem.x
-    if show_sol:
-        # bus generation: first nb_nodes
-        print("Bus: ", sol_v[:nb_nodes])
-        for line_idx, (node_1, node_2, c) in enumerate(pl):
-            print("Line", (node_1, node_2), "transm:", sol_v[nb_nodes + line_idx])
+        # lower/upper bound on x, transmission
+        lb = np.empty(nb_vars)  # lower bound
+        lb[:nb_nodes] = 0.
+        ub = np.empty(nb_vars)
+        ub[:nb_nodes] = np.array([gen for (node, gen, load, p) in self.gl])  # generation upper boundary
+        # line constraints ( abs(y) < c )
+        for node_1, node_2, c in self.pl:
+            y_pos = self.__find_pos_y(node_1, node_2)
+            lb[y_pos] = -c
+            ub[y_pos] = c
 
-    return problem.fun
+        # node constraints
+        for node_nb, generation, load, gen_price in self.gl:
+            vt_n = np.zeros(nb_vars)
+            incoming_l = self.__find_incoming(node_nb)
+            outgoing_l = self.__find_outgoing(node_nb)
+            vt_n[node_nb-1] = -1.
+            for node_inc in incoming_l:
+                vt_n[self.__find_pos_y(node_inc, node_nb)] = -1.
+            for node_out in outgoing_l:
+                vt_n[self.__find_pos_y(node_nb, node_out)] = 1.
+            A_ineq.append(vt_n)
+            b_ineq.append(-load)
 
+        # optimization vector - prices over nodes
+        opt_vec = np.zeros(nb_vars)
+        opt_vec[:nb_nodes] = np.array([p for (node_nb, gen, load, p) in self.gl])
 
-def computeLMP(gl, pl, show_sol=False, debug_ind=False, solver='glpk'):
-    """
-    Compute locational marginal pricing.
+        if A_eq:  # linprog cannon handle empty matrices
+            problem = linprog( opt_vec
+                             , A_ub = np.array(A_ineq)
+                             , b_ub = np.array(b_ineq)
+                             , A_eq = A_eq
+                             , b_eq = b_eq
+                             , bounds = list(zip(lb, ub)) )
+        else:
+            problem = linprog( opt_vec
+                             , A_ub = np.array(A_ineq)
+                             , b_ub = np.array(b_ineq)
+                             , bounds=list(zip(lb, ub)) )
 
-    """
-    comp_basic = computeLoadDistribution(gl, pl, show_sol=show_sol)
-    lmp = np.empty(len(gl))
+        sol_v = problem.x
+        if show_sol:
+            # bus generation: first nb_nodes
+            print("Bus: ", sol_v[:nb_nodes])
+            for line_idx, (node_1, node_2, c) in enumerate(self.pl):
+                print("Line", (node_1, node_2), "transm:", sol_v[nb_nodes + line_idx])
 
-    for node_nb, generation, load, gen_price in gl:
-        gl_orig = copy.deepcopy(gl[node_nb-1])
-        gl[node_nb-1] = (node_nb, generation, load + 1., gen_price)
-        new_value = computeLoadDistribution(gl, pl, show_sol=show_sol)
-        gl[node_nb-1] = gl_orig
-        lmp[node_nb-1] = new_value - comp_basic
+        return problem.fun
 
-    return lmp
+    def compute_lmp(self, show_sol=False):
+        """ Compute locational marginal pricing.
+
+        """
+
+        comp_basic = self.compute_load_distribution(show_sol=show_sol)
+        lmp = np.empty(len(self.gl))
+
+        for node_nb, generation, load, gen_price in self.gl:
+            gl_orig = copy.deepcopy(self.gl[node_nb-1])
+            self.gl[node_nb-1] = (node_nb, generation, load + 1., gen_price)
+            new_value = self.compute_load_distribution(show_sol=show_sol)
+            self.gl[node_nb-1] = gl_orig
+            lmp[node_nb-1] = new_value - comp_basic
+
+        return lmp
--- tests/test_lmp.py
+++ tests/test_lmp.py
@@ -1,37 +1,46 @@
+
 import numpy as np
+from lmp import LMP
 
-import lmp
-
-gl_simple = [(1, 150., 0., 10.), (2, 150., 120., 15.)]
-pl_simple = [(1, 2, 100.)]
-# res = lmp.comp_val(gl_simple, pl_simple, show_sol=True)
-# print res
-# print lmp.comp_lmp(gl_simple, pl_simple, show_sol=False)
-
-gl_1 = [(1, 500., 0., 10.), (2, 0., 75., 0.), (3, 500., 325, 20.)]
-pl_1 = [(1, 2, 300.), (2, 3, 40.), (1, 3, 200.)]
-# print lmp.comp_val(gl_1, pl_1, debug_ind=False)
-# print lmp.comp_lmp(gl_1, pl_1, show_sol=False)
+from unittest import TestCase
 
 
-# complicated network # (node, gen, load, price)
-gl_2 = [(1, 300., 100., 10.), (2, 0., 100., 0.), (3, 0., 100., 0.),
-        (4, 300., 100., 20.), (5, 0., 100., 0.)]
-pl_2 = [(1, 2, 500.), (1, 3, 200.), (2, 3, 200.), (3, 4, 500.), (4, 5, 150.)]
-print (lmp.comp_val(gl_2, pl_2))
-print (lmp.comp_lmp(gl_2, pl_2, show_sol=True))
+class LMPTest(TestCase):
 
+    def test_1(self):
+        gl_simple = [(1, 150., 0., 10.), (2, 150., 120., 15.)]
+        pl_simple = [(1, 2, 100.)]
+        # res = lmp.comp_val(gl_simple, pl_simple, show_sol=True)
+        # print res
+        # print lmp.comp_lmp(gl_simple, pl_simple, show_sol=False)
 
-# large network
-N = 20
-gl_3 = zip(range(1, N+1), np.random.random(N) * 100.,
-           np.random.random(N) * 50.,
-           np.random.random(N) * 100.)  # (n, gen, load, p) , (2, 0., 100., 0.), (3, 0., 100., 0.),
-pl_3 = []
-for i in range(1, N+1):
-    for j in range(i+1, N+1):
-        pl_3.append((i, j, 2.))
-# print lmp.comp_lmp(gl_3, pl_3, show_sol=True, solver='glpk')
+        gl_1 = [(1, 500., 0., 10.), (2, 0., 75., 0.), (3, 500., 325, 20.)]
+        pl_1 = [(1, 2, 300.), (2, 3, 40.), (1, 3, 200.)]
+        # print lmp.comp_val(gl_1, pl_1, debug_ind=False)
+        # print lmp.comp_lmp(gl_1, pl_1, show_sol=False)
 
-# print lmp.find_pos_y(2,1, gl_2, pl_2)
-# print lmp.find_connected(5, pl_2)
+        # complicated network # (node, gen, load, price)
+        gl_2 = [(1, 300., 100., 10.), (2, 0., 100., 0.), (3, 0., 100., 0.),
+                (4, 300., 100., 20.), (5, 0., 100., 0.)]
+        pl_2 = [(1, 2, 500.), (1, 3, 200.), (2, 3, 200.), (3, 4, 500.), (4, 5, 150.)]
+
+        lmp_simple = LMP(gl_simple, pl_simple)
+        lmp_2      = LMP(gl_2, pl_2)
+        print (lmp_simple.compute_lmp())
+        print (lmp_2.compute_lmp(show_sol=True))
+
+    def test_large_network(self):
+
+        # large network
+        N = 20
+        gl_3 = zip(range(1, N+1), np.random.random(N) * 100.,
+                   np.random.random(N) * 50.,
+                   np.random.random(N) * 100.)  # (n, gen, load, p) , (2, 0., 100., 0.), (3, 0., 100., 0.),
+        pl_3 = []
+        for i in range(1, N+1):
+            for j in range(i+1, N+1):
+                pl_3.append((i, j, 2.))
+        # print lmp.comp_lmp(gl_3, pl_3, show_sol=True, solver='glpk')
+
+        # print lmp.find_pos_y(2,1, gl_2, pl_2)
+        # print lmp.find_connected(5, pl_2)
