3c26ba3b2086b62fc62a16141be7b04d89306885
brumen <gorazd.brumen@gmail.com>
2019-09-28 20:16:46 -0400

    Adding the data for c0c1c2 volatility parametrization, mostly testing.

---
+++ data/c0c1c2vols.py
@@ -0,0 +1,10 @@
+# volatility
+
+import datetime
+
+from ds import get_forward_curve
+
+wti2_dates, _ = get_forward_curve('WTI', datetime.date(2015, 4, 10))
+wti2_vol = {wti2_date: (0.1, 0.2, 0.3, 0.4, 0.5)  # c0, c1, c2, theta, alpha
+            for wti2_date in wti2_dates}
+
