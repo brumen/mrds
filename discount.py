b70d5d3182a71d9b612ad6a369b36cbbffa269af
brumen <gorazd.brumen@gmail.com>
2019-10-14 12:01:33 -0400

    refactoring Discount curve classes.

--- discount.py
+++ discount.py
@@ -1,75 +1,51 @@
+#
+#  Functions concerned w/ discount curve
+#
+
 import datetime
 import numpy    as np
 import QuantLib as ql
 
 from scipy.interpolate import splrep, splev
 
-from ds import get_forward_curve  # , fwd_codes
+from ds import get_forward_curve
 
 
-def read_discount_curve(mkt_date: datetime.date, dcf=365.25):
-    """ Returns the discount curve on date date_.
+class DiscountCurve:
 
-    :param mkt_date: market date
-    :param dcf: day-count factor.
-    :returns:
-    """
+    @staticmethod
+    def discount_function(mkt_date : datetime.date, dcf = 365.25):
+        """ Returns the discount function for the market date.
 
-    disc_tenors, discount_yields = get_forward_curve('DISCOUNT', mkt_date)
-    diffs = [tenor - mkt_date for tenor in disc_tenors]
-    disc_tenors_numeric = np.array([float(elt.days) for elt in diffs]) / dcf
-    discount_yields = np.array([float(x) for x in discount_yields])
+        :param mkt_date: market date
+        :param dcf: day-count factor.
+        :returns:
+        """
 
-    return splrep( disc_tenors_numeric
-                 , np.exp(-disc_tenors_numeric * discount_yields))  # interpolation function
+        disc_tenors, discount_yields = get_forward_curve('DISCOUNT', mkt_date)
+        diffs = [tenor - mkt_date for tenor in disc_tenors]
+        disc_tenors_numeric = np.array([float(elt.days) for elt in diffs]) / dcf
+        discount_yields = np.array([float(x) for x in discount_yields])
 
+        interpolated_curve = splrep(disc_tenors_numeric, np.exp(-disc_tenors_numeric * discount_yields))  # interpolation function
 
-def read_discount_curve_quantlib(mkt_date: datetime.date, dcf=365.25):
-    """ Returns the discount curve for a particular market date. Usues quantlib to construct it
+        return lambda time_diff: splev(time_diff, interpolated_curve)
 
-    :param mkt_date: market date
-    :param dcf: day-count factor.
-    :returns:
-    """
+    @staticmethod
+    def discount_function_ql(mkt_date : datetime.date):
+        """ Returns the Quantlib discount function for the market date.
 
-    eonia_rates = get_forward_curve('DISCOUNT_QL', mkt_date)
-    ql.Settings.instance().evaluationDate = ql.Date(mkt_date)
-    eonia_helpers = [ql.OISRateHelper(2, ql.Period(*tenor), ql.QuoteHandle(ql.SimpleQuote(rate/100)), ql.Eonia() )
-                     for rate, tenor in eonia_rates]
+        :param mkt_date: market date
+        :param dcf: day-count factor.
+        :returns:
+        """
 
-    eonia_curve_flat = ql.PiecewiseFlatForward(0, ql.TARGET(), eonia_helpers, ql.Actual365Fixed())
-    eonia_curve_flat.enableExtrapolation()
+        eonia_rates = get_forward_curve('DISCOUNT_QL', mkt_date)
+        ql.Settings.instance().evaluationDate = ql.Date(mkt_date)
+        eonia_helpers = [ql.OISRateHelper(2, ql.Period(*tenor), ql.QuoteHandle(ql.SimpleQuote(rate / 100)), ql.Eonia())
+                         for rate, tenor in eonia_rates]
 
-    return eonia_curve_flat
+        eonia_curve_flat = ql.PiecewiseFlatForward(0, ql.TARGET(), eonia_helpers, ql.Actual365Fixed())
+        eonia_curve_flat.enableExtrapolation()
 
-
-# TODO: THESE TWO FUNCTIONS SHOULD GO OUT.
-
-def DF_single( mktDate: datetime.date
-             , fwdDate: datetime.date
-             , dcf           = 365.25
-             , discountCurve = None ) -> float:
-    """ Discount factor for a single forward date fwd_date.
-
-    :param mktDate: market date of the yield curve.
-    :param fwdDate: forward date.
-    :param dcf: date count convention
-    :param discountCurve: discount curve, if provided.
-    """
-
-    return splev( (fwdDate - mktDate).days / dcf
-                , discountCurve if discountCurve else read_discount_curve(mktDate) )
-
-
-def DF(mkt_date: datetime.date, fwd_date):
-    """ Discount factor from date date_ till date_fut.
-
-    :param mkt_date: market date for discount factor
-    :param fwd_date: forward date to which the discount is constructed.
-    :returns: discount between two dates
-    """
-
-    if isinstance(fwd_date, list):
-        return [DF_single(mkt_date, date_f) for date_f in fwd_date]
-
-    return DF_single(mkt_date, fwd_date)
+        return lambda time_diff: eonia_curve_flat.discount(time_diff)
