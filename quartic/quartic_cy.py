d83be679c252277173c47c7f93ec7f4b34dde944
brumen <gorazd.brumen@gmail.com>
2019-10-26 10:21:05 -0400

    cleanup.

--- quartic/quartic_cy.py
+++ quartic/quartic_cy.py
@@ -1,12 +1,9 @@
-import config 
-from distutils.core import setup
+from distutils.core      import setup
 from distutils.extension import Extension
-from Cython.Distutils import build_ext
+from Cython.Distutils    import build_ext
 
-ext_modules = [Extension("quartic_cy", ["quartic_cy.pyx"])]
-setup(name = 'solution of the quartic equation (cython)',
-      cmdclass = {'build_ext': build_ext},
-      ext_modules = ext_modules)
-
+setup(name        = 'solution of the quartic equation (cython)',
+      cmdclass    = {'build_ext': build_ext},
+      ext_modules = [Extension("quartic_cy", ["quartic_cy.pyx"])] )
 
 # compile using python quartic_cy.py build_ext --inplace
