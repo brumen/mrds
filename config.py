8d32d40041d03a38ef7054fcaaa7b1d04b2cadd6
brumen <gorazd.brumen@gmail.com>
2019-10-26 09:48:32 -0400

    cleanup.

--- config.py
+++ config.py
@@ -2,27 +2,26 @@
 from subprocess import Popen, PIPE
 
 # cuda check
-output, _ = Popen( ['prime-select', 'query']
-                 , stdin  = PIPE
-                 , stdout = PIPE
-                 , stderr = PIPE).communicate()
-
-# CUDA_PRESENT = True if output == b'nvidia\n' else False
 CUDA_PRESENT = False
+# CUDA_PRESENT = True if output == b'nvidia\n' else False
+
+if CUDA_PRESENT:
+    output, _ = Popen( ['prime-select', 'query']
+                     , stdin  = PIPE
+                     , stdout = PIPE
+                     , stderr = PIPE).communicate()
+    import pycuda.autoinit
+    from pycuda.compiler import SourceModule
+    pycuda.compiler.DEFAULT_NVCC_FLAGS = ['--use_fast_math']
+
 
 # adding various paths
 prod_dir = '/home/brumen/work/mrds/'
 work_dir = prod_dir
 
-
 # cython params
 cython_include_dirs    = []
 cython_extra_link_args = []
 
-if CUDA_PRESENT:  # cuda modules
-    import pycuda.autoinit
-    from pycuda.compiler import SourceModule
-    pycuda.compiler.DEFAULT_NVCC_FLAGS = ['--use_fast_math']
-
 # for One-period dispatch
-opd1FuelComplete = 'opd_1fuel_cu_by_block_new.c'
+opd_1_fuel_cuda_code = 'opd_1fuel_cu_by_block_new.c'
