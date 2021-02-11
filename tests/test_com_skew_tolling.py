0de2df7ccf633a72707467afab84947d42b4de9a
brumen <gorazd.brumen@gmail.com>
2021-02-11 10:38:12 -0500

    adding test for com tolling skew model.

---
+++ tests/test_com_skew_tolling.py
@@ -0,0 +1,28 @@
+#
+# tests for com_skew_tolling
+import datetime
+
+from unittest import TestCase
+
+from mrds.tolling.com_skew_tolling import ComSkewTolling
+
+
+class ComSkewTollingTest(TestCase):
+
+    MKT_DATE = datetime.date(2015, 4, 1)
+
+    def test_basic(self):
+        """ Example of how to use ComSkewTolling class.
+        """
+
+        com_skew = ComSkewTolling.from_db(self.MKT_DATE
+                                         , ['WTI', 'BRENT']
+                                         , ['WTI', 'BRENT']
+                                         , ['WTI', 'BRENT']
+                                         , hours_partition = { 'WEEKDAY': [('WTI', 8) , ('BRENT', 16), ]
+                                                             , 'WEEKEND': [('WTI', 16), ('BRENT', 8 ), ] })
+
+        print(1+1)
+
+        self.assertTrue(True)
+
