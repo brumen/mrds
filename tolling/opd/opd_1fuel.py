7f3c434dc214a12b1a20fca713fb3c5f65099501
brumen <gorazd.brumen@gmail.com>
2021-02-23 11:23:20 -0500

    Fixing db lookups.

--- tolling/opd/opd_1fuel.py
+++ tolling/opd/opd_1fuel.py
@@ -33,18 +33,6 @@ def opd_1fuel(power_prices     : np.ndarray
     :param cashflow: vector of one period cashflow for every path
     """
 
-    # unpacking of params
-    # hr_at_max, hr_at_min, max_cap, min_disp, \
-    #     start_fuel, start_fuel_cold, \
-    #     add_fuel_cost, VC, ramp_rate, \
-    #     shutdown_sp_in, \
-    #     min_downtime, min_runtime, \
-    #     fixed_startup_cost, fixed_startup_cost_cold,  \
-    #     max_monthly_starts, \
-    #     cold_startup, startup_horizon, shutdown_horizon, \
-    #     ramp_up_sp_in, ramp_down_sp_in, \
-    #     ramp_up_cost, ramp_down_cost, ramp_up_horizon, ramp_down_horizon = tolling_params
-
     hr_at_max       = tolling_params['hrAtMax']
     hr_at_min       = tolling_params['hrAtMin']
     max_cap         = tolling_params['maxCap']
@@ -84,7 +72,7 @@ def opd_1fuel(power_prices     : np.ndarray
     ramp_down_to_min_cost = generation_larger_mindisp * (ramp_down_sp_in + ramp_down_cost / min_disp / ramp_down_horizon)
     run_at_min_index = max_cap * (power_prices - optimal_marginal_cost_at_max - ramp_up_to_max_cost) < \
                        min_disp * (power_prices - optimal_marginal_cost_at_min - ramp_down_to_min_cost)
-    not_run_at_min_index = ~run_at_min_index
+    # not_run_at_min_index = ~run_at_min_index
 
     # compute total startup costs
     is_cold_start     = curr_state['hours_shut'] >= cold_startup
@@ -102,8 +90,11 @@ def opd_1fuel(power_prices     : np.ndarray
                                                (is_startup_profitable & (curr_decision['force_start'] == 1)))
 
     # compute shutdown
-    actual_gen_profit = run_at_min_index * (power_prices - optimal_marginal_cost_at_min) * min_disp + \
-                        not_run_at_min_index * (power_prices - optimal_marginal_cost_at_max) * max_cap
+    #actual_gen_profit = run_at_min_index * (power_prices - optimal_marginal_cost_at_min) * min_disp + \
+    #                    not_run_at_min_index * (power_prices - optimal_marginal_cost_at_max) * max_cap
+    actual_gen_profit = np.where( run_at_min_index
+                                , (power_prices - optimal_marginal_cost_at_min) * min_disp
+                                , (power_prices - optimal_marginal_cost_at_max) * max_cap )
     shutdown_gen_profit = shutdown_horizon * actual_gen_profit
     shut_cost_sp = shutdown_horizon * shutdown_sp_in * max_cap
     is_shutdown_profitable = shutdown_gen_profit < - (fixed_and_fuel_startup_cost + shut_cost_sp)
@@ -111,26 +102,34 @@ def opd_1fuel(power_prices     : np.ndarray
                                                (is_shutdown_profitable & (curr_decision['force_shut'] == 1)))
     # compute dispatch
     not_state_state = ~state_state
-    new_state       = (state_state & (~do_shutdown)) | (not_state_state & do_startup)
+    # new_state       = (state_state & (~do_shutdown)) | (not_state_state & do_startup)
+    new_state = np.where(state_state, ~do_shutdown, do_startup)
     state_change    = new_state != state_state
 
     # accounting (- on curr_state is because curr_state is -1)
-    curr_generation = new_state * (max_cap * not_run_at_min_index + run_at_min_index * min_disp)
+    # curr_generation = new_state * (max_cap * not_run_at_min_index + run_at_min_index * min_disp)
+    curr_generation = new_state * np.where(run_at_min_index, min_disp, max_cap)
     generation_change = curr_generation - curr_state['generation']
     ramping_adjustment = (0.5 / (ramp_rate * hours_in_block)) * np.abs(generation_change) * generation_change
     curr_generation -= ramping_adjustment
     curr_energy = curr_generation * hours_in_block
     revenue = curr_energy * power_prices
-    variable_cost = VC * curr_energy
-    actual_heat_rate = run_at_min_index * hr_at_min + not_run_at_min_index * hr_at_max
-    fuel_cost = curr_energy * (fuel_prices + add_fuel_cost) * actual_heat_rate
+    # variable_cost = VC * curr_energy
+    # actual_heat_rate = run_at_min_index * hr_at_min + not_run_at_min_index * hr_at_max
+    # actual_heat_rate = np.where(run_at_min_index, hr_at_min, hr_at_max)
+    fuel_cost = curr_energy * (fuel_prices + add_fuel_cost) * np.where(run_at_min_index, hr_at_min, hr_at_max)  # last expression is actual heat rate
 
     not_new_state = ~new_state
     starts = new_state & not_state_state  # curr_state > state_state
     shuts = not_new_state & state_state
 
-    startup_cost = (is_cold_start & starts) * (fixed_startup_cost_cold + fuel_prices * start_fuel_cold) + \
-                   (is_not_cold_start & starts) * (fixed_startup_cost + fuel_prices * start_fuel)
+    # startup_cost = (is_cold_start & starts) * (fixed_startup_cost_cold + fuel_prices * start_fuel_cold) + \
+    #                (is_not_cold_start & starts) * (fixed_startup_cost + fuel_prices * start_fuel)
+    startup_cost = np.where( starts
+                           , np.where( is_cold_start
+                                     , fixed_startup_cost_cold + fuel_prices * start_fuel_cold
+                                     , fixed_startup_cost + fuel_prices * start_fuel )
+                           , 0 )
 
     ramp_cost = ((~starts) & (generation_change > SMALL_EPS  )) * ramp_up_cost + \
                  ((~shuts ) & (generation_change < - SMALL_EPS)) * ramp_down_cost
@@ -139,13 +138,16 @@ def opd_1fuel(power_prices     : np.ndarray
     # totalCost = fuel_cost + variable_cost + startup_cost + ramp_cost
     # cashflow = revenue - totalCost
     # cashflow[:] = cashflow
-    opd_avx.add4(revenue, fuel_cost, variable_cost, startup_cost, ramp_cost, cashflow, nb_paths)
+    # VC * curr_energy = variable cost
+    opd_avx.add4(revenue, fuel_cost, VC * curr_energy, startup_cost, ramp_cost, cashflow, nb_paths)
 
     # new unit state
     curr_state['hours_in_state']  = curr_state['hours_in_state'] * (~state_change) + hours_in_block
     curr_state['generation']      = curr_generation
     curr_state['total_starts']   += starts
-    curr_state['hours_shut']      = not_new_state * (curr_state['hours_shut'] + hours_in_block)
-    curr_state['hours_run']       = new_state * (curr_state['hours_run'] + hours_in_block)
+    # curr_state['hours_shut']      = not_new_state * (curr_state['hours_shut'] + hours_in_block)
+    curr_state['hours_shut']      = np.where(not_new_state, curr_state['hours_shut'] + hours_in_block, 0)
+    # curr_state['hours_run']       = new_state * (curr_state['hours_run'] + hours_in_block)
+    curr_state['hours_run']       = np.where(new_state, curr_state['hours_run'] + hours_in_block, 0)
     curr_state['global_starts']  += starts
     curr_state['state']           = new_state
