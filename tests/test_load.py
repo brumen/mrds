85aa68d541c4a622ff1968b7356d8cea5f3d9535
brumen <gorazd.brumen@gmail.com>
2021-07-09 08:27:23 -0400

    cleaning up.

--- tests/test_load.py
+++ tests/test_load.py
@@ -1,24 +1,37 @@
 import numpy as np
-from load import Load
+from mrds.load import Load
 
-gl_compl = [(1, 100, 30), (2, 100, 35),
-            (5, -150, 70), (6, -150, 50)]
-p_compl = [(1, 3, 100, 1),
-           (2, 4, 100, 1),
-           (3, 4, 200, 1),
-           (3, 5, 120, 1),
-           (4, 6, 200, 1)]
-
-gl_compl_pos = {1: np.array([0., 0.]),
-                2: np.array([0., -1.]),
-                3: np.array([1., 0.]),
-                4: np.array([1., -1.]),
-                5: np.array([2., 0.]),
-                6: np.array([2., -1.])}
+from unittest import TestCase
 
 
-load_obj = Load(gl_compl, p_compl)
+class LoadTest(TestCase):
 
-res_compl = load_obj.cons_lp()
-print(res_compl)
-load_obj.draw_network(res_compl['solution_edges'], pos=gl_compl_pos)
+    gl_compl = [(1, 100, 30), (2, 100, 35),
+                (5, -150, 70), (6, -150, 50)]
+
+    p_compl = [(1, 3, 100, 1),
+               (2, 4, 100, 1),
+               (3, 4, 200, 1),
+               (3, 5, 120, 1),
+               (4, 6, 200, 1)]
+
+    gl_compl_pos = {1: np.array([0., 0.]),
+                    2: np.array([0., -1.]),
+                    3: np.array([1., 0.]),
+                    4: np.array([1., -1.]),
+                    5: np.array([2., 0.]),
+                    6: np.array([2., -1.])}
+
+    load_obj = Load(gl_compl, p_compl)
+
+    # TODO: FINISH THIS TEST.
+    def test_smoke_test(self):
+        """ Runs the smoke test for the load optimizer.
+        """
+
+        res_compl = self.load_obj.cons_lp()
+
+        self.load_obj.draw_network( res_compl['solution_edges']
+                                  , pos = self.gl_compl_pos )
+
+        self.assertTrue(True)
