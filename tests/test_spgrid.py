e5423d49790e2e38897a5e1fa741d14575d1378d
brumen <gorazd.brumen@gmail.com>
2021-08-03 12:24:17 -0400

    cleanup.

--- tests/test_spgrid.py
+++ tests/test_spgrid.py
@@ -1,41 +1,33 @@
-import scipy
-import scipy.integrate
-import scipy.linalg
-import unittest
+""" Tests for sparse grid integration.
+"""
+
 import numpy as np
-
-# testing sparse grids
-import mrds.sg as sg
-
 import scipy.integrate
 
+from unittest import TestCase
 
-class SparseGridTests(unittest.TestCase):
+from mrds.sg import sg_quad
+
+
+class SparseGridTests(TestCase):
 
     def test_simple_1(self):
-        A = sg.sg_p (1,5)
-        B = sg.sg_w (1,5)
-        f = lambda x: sum (x**2)
-        res1 = sg.sg_quad (1, 15, f)
-        res2 = scipy.integrate.quad (f, 0., 1.)[0]
-        res3 = scipy.linalg.norm(res1-res2) < 1.
+        """ Test the sparse grid quadrature in 1 dim.
+        """
 
-        # really WEAK test
-        self.assertTrue( res3 )
+        res_1 = sg_quad (1, 15, lambda x: x**2)
+        res_2 = scipy.integrate.quad (lambda x: x**2 * np.exp(-x**2/2)/np.sqrt(2. * np.pi), -np.inf, np.inf)[0]
+
+        self.assertAlmostEqual( res_1, res_2, 2 )
 
     def test_sg_quad(self):
         """ Integrate (2 * x[0]**2 + 3 * x[1]**3) * e^(-x**2 - y**2) / 2 pi
         """
 
         g = lambda x: 2 * x[0]**2 + 3 * x[1]**3
-        v1 = sg.sg_quad(2, 5, g)
-        v2 = scipy.integrate.dblquad(lambda x, y: g([x, y]) * np.exp(-x**2/2. - y**2/2.),
-                         -np.inf, np.inf,
-                         lambda x: -np.inf, lambda x: np.inf)[0] / (2 * np.pi)
+        v_1 = sg_quad(2, 5, g)
+        v_2 = scipy.integrate.dblquad( lambda x, y: g([x, y]) * np.exp(-x**2/2. - y**2/2.)
+                                    , -np.inf, np.inf
+                                    , -np.inf, np.inf )[0] / (2 * np.pi)
 
-        self.assertLess(np.abs(v1 - v2), 1.e-5)
-
-
-def main():
-    suite = unittest.TestLoader().loadTestsFromTestCase(SparseGridTests)
-    unittest.TextTestRunner(verbosity=2).run(suite)
+        self.assertAlmostEqual(v_1, v_2, 5)
